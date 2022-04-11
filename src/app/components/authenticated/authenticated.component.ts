048e6920a524c7c53679f06c36d1c8de5fd8194f
Stole <stojancekrstevski0@gmail.com>
2022-04-11 15:23:33 +0200

    NavBar

--- src/app/components/authenticated/authenticated.component.ts
+++ src/app/components/authenticated/authenticated.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-authenticated',
   templateUrl: './authenticated.component.html',
-  styleUrls: ['./authenticated.component.css']
+  styleUrls: ['./authenticated.component.css','../../app.component.css']
 })
 export class AuthenticatedComponent implements OnInit {
 
