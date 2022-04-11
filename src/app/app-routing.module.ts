4bb21e02d0d0c68dedcc19d0c7ff4f0aca40e441
Stole <stojancekrstevski0@gmail.com>
2022-04-11 12:02:40 +0200

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticatedComponent } from './components/authenticated/authenticated.component';
+import { LoginComponent } from './components/login/login.component';
+import { NewsFeedComponent } from './components/news-feed/news-feed.component';
+import { RegisterComponent } from './components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '',
+    component: AuthenticatedComponent, 
+    children: [
+      { path: '', component: NewsFeedComponent },
+    ]
+  },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NewsFeedComponent } from './components/news-feed/news-feed.component'
+import { AuthenticatedComponent } from './components/authenticated/authenticated.component';
+import { SideSectionComponent } from './components/side-section/side-section.component';
+import { PostComponent } from './components/post/post.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    RegisterComponent,
+    NewsFeedComponent,
+    AuthenticatedComponent,
+    SideSectionComponent,
+    PostComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
