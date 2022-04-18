da343e674f741d82215c2b4fe940056859cb8131
DavidTrajkovski <davidtrajkovski11@gmail.com>
2022-04-18 22:12:30 +0200

    started editing profile connection

--- src/app/components/edit-profile/edit-profile.component.ts
+++ src/app/components/edit-profile/edit-profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {Router} from "@angular/router";
+import {UserService} from "../../services/user.service";
 
 @Component({
   selector: 'app-edit-profile',
@@ -7,9 +10,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditProfileComponent implements OnInit {
 
-  constructor() { }
+  editProfile!: FormGroup
+
+  constructor(private userService: UserService,
+              private router: Router,
+              private formBuilder: FormBuilder) {
+  }
 
   ngOnInit(): void {
+    this.editProfile = this.formBuilder.group({
+      name: '',
+      surname: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+      description: '',
+      image: '',
+    });
+  }
+
+  onSubmit(): void {
+    if (this.editProfile.invalid) {
+      return;
+    }
+
+    let formData = new FormData();
+    formData.append("name", this.editProfile.controls['name'].value)
+    formData.append("surname", this.editProfile.controls['surname'].value)
+    formData.append("email", this.editProfile.controls['email'].value)
+    formData.append("password", this.editProfile.controls['password'].value)
+    formData.append("confirmPassword", this.editProfile.controls['confirmPassword'].value)
+    formData.append("description", this.editProfile.controls['description'].value)
+    formData.append("image", this.editProfile.get('image')!!.value)
+
+    this.userService.editProfile(formData)
+      .subscribe({
+        next: profile => {
+          this.router.navigate(['/profile/posts'])
+        },
+        error: () => {
+          console.log('error updating profile')
+        }
+      })
+  }
+
+  onFileChange(event: Event){
+    const element = event.currentTarget as HTMLInputElement;
+    let fileList: FileList | null = element.files;
+    if(fileList!=null && fileList.length > 0){
+      const file =fileList[0];
+      this.editProfile.get('image')!!.setValue(file);
+    }
   }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,18 +1,20 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {DomSanitizer} from "@angular/platform-browser";
 import {Observable} from "rxjs";
-import {Post} from "../models/Post";
-import {User} from "../models/User";
+import {Response} from "../models/Response";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private http: HttpClient,) {
+  constructor(private http: HttpClient,
+              private sanitizer: DomSanitizer,) {
   }
 
-  findAllBySearchText(searchText: string): Observable<User[]> {
-    return this.http.get<User[]>(`/api/users/find-all-by-search-text?searchText=${searchText}`);
+  editProfile(formData: FormData): Observable<Response<any>> {
+    return this.http.put<Response<any>>('/api/users/edit-profile', formData);
   }
+
 }
